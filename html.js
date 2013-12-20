6c7eabfd7eab88748ea74f734274b6a87c351558
Nicholas Rawlings <nicholas@dejami.com>
2013-12-20 16:11:31 -0500

    Adds the ability to strip extra whitespace from between attributes and between tags and text

--- html.js
+++ html.js
@@ -15,18 +15,31 @@ define(['text'], function(textPlugin) {
 			
 			textPlugin.get(req.toUrl(file), function(html) {
 			
-				if (config.config.html.stripComments === true) {
+				console.group(name);
+				console.log('Original:', html);
+				
+				if (config.config.html.comments === true) {
 					html = this.stripComments(html);
 				}
 				
-				if (config.config.html.stripWhitespace === true) {
+				if (config.config.html.whitespaceBetweenTags === true) {
 					html = this.stripWhitespaceBetweenTags(html);
+				}
+				
+				if (config.config.html.whitespaceBetweenTagsAndText === true) {
+					html = this.stripWhitespaceBetweenTagsAndText(html);
+				}
+				
+				if (config.config.html.whitespaceWithinTags === true) {
+					html = this.stripWhitespaceWithinTags(html);
 				}
 				
 				if (config.isBuild) {
 					buildText[name] = textPlugin.jsEscape(html);
 				}
-			
+				
+				console.log('Final:', html);
+				console.groupEnd();
 				onLoad(html);
 				
 			}.bind(this), onLoad.error);
@@ -44,12 +57,52 @@ define(['text'], function(textPlugin) {
 		
 		
 		stripComments: function(html) {
-			return html.replace(/<!--[\n\r\s]*?-->/gm, '');
+			return html.replace(/<!--(.|[\n\r])*?-->/gm, '');
 		},
 		
 		
 		stripWhitespaceBetweenTags: function(html) {
 			return html.replace(/>[\n\r\s]+</gm, '><');
+		},
+		
+		
+		stripWhitespaceBetweenTagsAndText: function(html) {
+			return html.replace(/>[\n\r\s]+/gm, '>').replace(/[\n\r\s]+</gm, '<');
+		},
+		
+		
+		stripWhitespaceWithinTags: function(html) {
+			var tagPattern = /<([^>"']*?|"[^"]*?"|'[^']*?')+>/g,
+				attrPattern = /([^\0\n\r\s"'>\/=]+)(?:\s*=\s*([^\n\r\s"'=><`]+|"[^"]*"|'[^']*'))?/gi,
+				lastIndex = 0,
+				result = '',
+				match,
+				tag;
+			
+			while ((match = tagPattern.exec(html)) !== null) {
+				
+				// Copy text between the beginning of this match and the end of the last one
+				result += html.substring(lastIndex, match.index);
+				tag = match[0];
+				
+				if (/^<[^\/]/.test(tag)) {  // It's a start tag
+					var attrs = tag.match(attrPattern),
+						start = attrs.shift(),
+						end = /\/>$/.test(tag) ? '/>' : '>';
+					
+					result += start + attrs.map(function(attr) {
+						return attr.replace(attrPattern, ' $1=$2');
+					}).join('') + end;
+				}
+				
+				else {  // It's an end tag
+					result += tag.replace(/[\n\r\s]+/g, '');
+				}
+				
+				lastIndex = tagPattern.lastIndex;
+			}
+			
+			return result + html.substring(lastIndex);
 		}
 		
 	};
